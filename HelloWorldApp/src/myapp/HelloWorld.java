43b603a65d79da812263540c0da70f1ea6c8e20b
Andres Medaglia <andres.medaglia@gmail.com>
2018-12-08 17:18:35 -0500

    Cambie cuenta a gmail.

--- HelloWorldApp/src/myapp/HelloWorld.java
+++ HelloWorldApp/src/myapp/HelloWorld.java
@@ -15,6 +15,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hello World!"); // Display the string.
+        System.out.println("Hola mundo Paquito!!"); // Display the string.
 	}
 
 }
